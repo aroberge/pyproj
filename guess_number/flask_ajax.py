fc35fd5948dc7fa7a2ff327d516c6486ab8a257e
André Roberge <andre.roberge@gmail.com>
2015-01-22 19:55:10 -0400

    splitting html content and simplyfing

--- guess_number/flask_ajax.py
+++ guess_number/flask_ajax.py
@@ -2,50 +2,22 @@
 
 from flask import Flask, request
 
+
+with open("index.html", 'r') as f:
+    html_content = f.read()
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return html_content  # see below
+    return html_content
 
 
-@app.route('/_double')
+@app.route('/ajax_double')
 def double_number():
     return str(request.args.get('number', 0, type=int)*2)
 
-
-# For simplicity, we'll use jQuery
-html_content = '''
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script type=text/javascript>
-      $(function() {
-        $('#calculate').bind('click', function() {
-            $.ajax({url:'/_double',
-                      data:"number="+$('input[name="a"]').val()
-                   }).done(
-                        function(data) { $("#result").text(data);
-                         });
-          return false;
-        });
-      });
-    </script>
-  </head>
-  <body>
-    <h1>Double a number using ajax</h1>
-
-    <form>
-      <input type="text" size="5" name="a">
-      <button id="calculate">Calculate double:</button>
-      <span id="result">?</span>
-    </form>
-
-  </body>
-</html>
-'''
 
 if __name__ == '__main__':
     app.run(debug=True)
