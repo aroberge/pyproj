d77b01b33e6e7947f695fe5e4c6d1b13d5cd8069
André Roberge <andre.roberge@gmail.com>
2015-01-22 20:12:12 -0400

    reorganizing file, adding comments

--- guess_number/flask_ajax.py
+++ guess_number/flask_ajax.py
@@ -3,20 +3,34 @@
 from flask import Flask, request
 
 
+def double(n):
+    return 2 * n
+
 with open("index.html", 'r') as f:
     html_content = f.read()
+
+# ----  flask related stuff follows----------------
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    '''Returned by default at the start of the program'''
     return html_content
 
 
 @app.route('/ajax_double')
 def double_number():
-    return str(request.args.get('number', 0, type=int)*2)
+    '''retrieves result of ajax query and perform custom function
+
+       The url returned will be of the form:
+           /ajax_double?number=X
+       where X is the number we wish to double
+    '''
+    number = request.args.get('number', 0, type=int)
+    result = double(number)
+    return str(result)
 
 
 if __name__ == '__main__':
